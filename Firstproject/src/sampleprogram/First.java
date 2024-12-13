c6ee7390b4ec27183e80e75a4105b41401861afc
Tanishk Abinav <tanishk16148@gmail.com>
2024-12-13 18:29:23 +0530

    import

---
+++ Firstproject/src/sampleprogram/First.java
@@ -0,0 +1,21 @@
+package sampleprogram;
+
+public class First {
+	
+	
+	public void studentName1() {
+		System.out.println("Kenny");
+	}
+
+	public void studentName2() {
+		System.out.println("Vishal");
+	}
+	
+	
+	public static void main(String[]args) {
+		First obj = new First();
+		obj.studentName2();
+		obj.studentName1();
+		
+	}
+}
---
+++ Firstproject/src/sampleprogram/First2.java
@@ -0,0 +1,19 @@
+package sampleprogram;
+
+public class First2 {
+	public void studentName() {
+		System.out.println("Kenny");
+	}
+
+	public void studentdob() {
+		System.out.println("09/09");m	
+	}
+	
+	
+	public static void main(String[]args) {
+		First2 obj = new First2();
+		obj.studentName();
+		obj.studentdob();
+	}
+}
+
